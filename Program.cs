bde8c264b5243c02be6f37c32de9ee8424e0b2bb
happymonk <eric.schmeling@gmail.com>
2016-12-24 11:18:35 -0500

    added Encryption namespace and class placeholders

--- Program.cs
+++ Program.cs
@@ -5,12 +5,31 @@ using System.Threading.Tasks;
 
 namespace PassKeeper
 {
+    namespace EncryptDecrypt
+    {
+        class EncryptDecrypt
+        {
+
+            static void EncryptFile(string inputFile,
+            string outputFile,
+            string secretKey)
+            {
+
+            }
+            static void DecryptFile(string inputFile,
+                string outputFile,
+                string secretKey)
+            {
+
+            }
+        }
+    }
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.Write("Enter Encryption Password: ");
-            string password = Console.ReadLine();
+            Console.WriteLine("PassKeeper v0.1");
             Console.WriteLine("Press a key to exit...");
             Console.ReadKey();
         }
