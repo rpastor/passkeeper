ac3b4d1891eaa67e83307da3f6d63be50ed5d577
happymonk <eric.schmeling@gmail.com>
2017-01-18 23:16:23 -0500

    added getters and setters

--- src/PassKeeper/PassKeeper.cs
+++ src/PassKeeper/PassKeeper.cs
@@ -35,5 +35,35 @@ namespace PassKeeper
         {
             storage.DeletePassword(serviceName, encryptedUnlockSecret);
         }
+
+        public void AddLoginName(string serviceName, string loginName, string encryptedUnlockSecret)
+        {
+            storage.AddLoginName(serviceName, loginName, encryptedUnlockSecret);
+        }
+
+        public string GetLoginName(string serviceName, string encryptedUnlockSecret)
+        {
+            return storage.GetLoginName(serviceName, encryptedUnlockSecret);
+        }
+
+        public void AddPasswordHint(string serviceName, string passwordHint, string encryptedUnlockSecret)
+        {
+            storage.AddPasswordHint(serviceName, passwordHint, encryptedUnlockSecret);
+        }
+
+        public string GetPasswordHint(string serviceName, string encryptedUnlockSecret)
+        {
+            return storage.GetPasswordHint(serviceName, encryptedUnlockSecret);
+        }
+
+        public void AddSecurityQuestionAnswer(string serviceName, string securityQuestionAnswer, string encryptedUnlockSecret)
+        {
+            storage.AddSecurityQuestionAnswer(serviceName, securityQuestionAnswer, encryptedUnlockSecret);
+        }
+
+        public string[] GetSecurityQuestionAnswers(string serviceName, string encryptedUnlockSecret)
+        {
+            return storage.GetSecurityQuestionAnswers(serviceName, encryptedUnlockSecret);
+        }
     }
 }
